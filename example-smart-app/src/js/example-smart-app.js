910fbad1f944ad39c4145b9de5887f51bf8a867b
Jinzhong Zhang <31191315+zjz1983@users.noreply.github.com>
2019-10-18 16:54:03 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -79,6 +79,7 @@
       gender: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
+      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
