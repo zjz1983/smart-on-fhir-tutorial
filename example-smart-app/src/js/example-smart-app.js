6879b6b62446ed3c178454fe039f01143a823d53
Jinzhong Zhang <31191315+zjz1983@users.noreply.github.com>
2019-10-18 18:37:57 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -37,7 +37,7 @@
           }
 
           var height = byCodes('8302-2');
-          var weight = byCodes('8302-2');
+          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
